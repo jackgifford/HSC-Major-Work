f3e450ded8b943979f4dbf993eab5731e75601db
Jack Gifford <shmacktus@gmail.com>
2016-07-07 12:29:07 +1000

    New model & init of MazeSolveService stubs

---
+++ MajorWork.Logic/Models/Heuristic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MajorWork.Logic.Models
+{
+    class Heuristic
+    {
+        public int DeltaX { get; set; }
+        public int DeltaY { get; set; }
+        public int H { get; set; }
+    }
+
+
+}
---
+++ MajorWork.Logic/Services/MazeSolveService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MajorWork.Logic.Models;
+
+namespace MajorWork.Logic.Services
+{
+    public class MazeSolveService
+    {
+        private List<Mazepoints> OpenSet;
+        private List<Mazepoints> ClosedSet;
+
+        public MazeSolveService()
+        {
+
+            OpenSet.Add(new Mazepoints(0, 0, false)); //Add starting position to the open list
+            //Call HuersticCalculator for every node in the list
+            //But only process those that aren't walls
+        }
+
+        private void HeuristicCalculator(int x, int y, int finalX, int finalY) //Based off Manhattan Distance
+        {
+            var Manhattan = new Heuristic();
+            Manhattan.DeltaX = Math.Abs(x - finalX);
+            Manhattan.DeltaY = Math.Abs(y - finalY);
+            Manhattan.H = (Manhattan.DeltaX + Manhattan.DeltaY);
+        }
+
+        private void AStar()
+        {
+
+        }
+
+        private void BuildSolution()
+        {
+
+        }
+    }
+}
