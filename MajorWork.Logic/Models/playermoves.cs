0b52ed5cf3a8ac2ecff843550a70dc9b0748cd6c
shmacktus <shmacktus@gmail.com>
2016-08-24 16:28:13 +1000

    Quick tidy up

--- MajorWork.Logic/Models/playermoves.cs
+++ MajorWork.Logic/Models/playermoves.cs
@@ -6,14 +6,6 @@ using System.Threading.Tasks;
 
 namespace MajorWork.Logic.Models
 {
-    public class PlayerMoves
-    {
-        private MoveList _playerFields;
-
-        public MoveList PlayerFields { get { return _playerFields; } set { _playerFields = PlayerFields; } }
-
-
-    }
 
     public enum MoveList
     {
